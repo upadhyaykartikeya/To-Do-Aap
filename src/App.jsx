b39bb90432e0767621c4dfd7d59f0db794aca78c
upadhyaykartikeya <ukartik451@gmail.com>
2025-04-06 08:45:54 +0530

    navbar setup

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,39 @@ import { useState } from 'react'
 import Navbar from './component/Navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [todo, setTodo] = useState("")
+  const [todos, setTodos] = useState([])
+
+  const handleEdit = ()=> {
+    
+  }
+  const handleDelete = ()=> {
+
+  }
+  const handleAdd = ()=> {
+    setTodos([...todos, {todo, isCompleted: false}])
+    setTodo("")
+  }
 
   return (
     <>
     <Navbar/>
-    <div className="container mx-auto my-5 rounded-xl p-5 bg-stone-500">
-      <div className="flex ">
-        <h1>Your Todos</h1>
+    <div className="container mx-auto my-5 rounded-xl p-5 bg-stone-400 min-h-[80vh]">
+      <div className="addTodo my-5">
+        <h2 className='font-bold tex-lg'>Add To-do</h2>
+      <input onChange={handleChange} value={todo} className='w-1/2' type="text" />
+      <button onClick={handleAdd} className='bg-stone-700 hover:bg-stone-600 text-white rounded-md text-sm font-bold p-2 py-1 mx-6'>Add</button>
+      </div>
+      <h2 className=' text-lg font-bold'>Your Todos</h2>
+      <div className="todos">
+        <div className="todo flex">
+          <div className="text">myTodo</div>
+          <div className="buttons">
+            <button onClick={handleEdit} className='bg-stone-700 hover:bg-stone-600 text-white rounded-md text-sm font-bold p-2 py-1 mx-1'>Edit</button>
+            <button onClick={handleDelete} className='bg-stone-700 hover:bg-stone-600 text-white rounded-md text-sm font-bold p-2 py-1 mx-1'>Delete</button>
+          </div>
+        </div>
       </div>
     </div>
     </>
--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
         </div>
         <ul className="flex gap-8 mx-9">
             <li className='cursor-pointer hover:font-bold transition-all'>Home</li>
-            <li className='cursor-pointer hover:font-bold transition-all'>Your Task</li>
+            <li className='cursor-pointer hover:font-bold transition-all'>Your Tasks</li>
         </ul>
     </nav>
   )
